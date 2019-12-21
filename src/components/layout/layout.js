399720bfd1b65f76d5f576128f4c0ce31ea7eb7a
kidmeeno <arthurbosah@yahoo.com>
2019-12-21 17:35:29 +0100

    done with sidebar and layout structure

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import './layout.css'
-import Header from '../header/header'
+import './layout.css';
+import Header from '../header/header';
+import overViewIcon from '../../assets/images/overviewIcon.svg';
+import allPaymentIcon from '../../assets/images/wallet-43.svg';
+import reconcilePaymentIcon from '../../assets/images/wallet-43 (1).svg';
+import unReconsilePaymentIcon from '../../assets/images/wallet-43 (2).svg';
+import manuelSettingIcon from '../../assets/images/ic_fiber_manual_record_48px.svg';
+import merchantProfileIcon from '../../assets/images/single-01 2.svg';
+import allOrdersIcon from '../../assets/images/Group 7.1.svg';
+import pendingOrdersIcon from '../../assets/images/Group 7.svg';
+import reconciledOrdersIcon from '../../assets/images/Group 7.2.svg';
 
 
 const Layout = (props) => {
     return (
         <div className={"layout__wrapper"}>
-            <Header/>
+            <Header />
             <div className={"layout__body"}>
                 <div className={"side__nav__wrapper"}>
                     <div className={"generate__invoice__wrapper"}>
@@ -14,26 +23,103 @@ const Layout = (props) => {
                     </div>
                     <div className={"sidebar__nav__wrapper"}>
                         <ul>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
-                            <li className={"sidebar__li"}><span>Main</span></li>
+                            <li className={"sidebar__li"}>
+                                <div className={"nav__item__res"}><span>Main</span></div>
+                            </li>
+                            <li className={"sidebar__li"}>
+                                <div className={"active__nav__tag"}>
+                                    <div className={"nav__content__res"}>
+                                        <img src={overViewIcon} alt="" className={"nav__icons"} />
+                                        <span>Overview</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className={"sidebar__li"}>
+                                {/* <div className={"nav__item__res"}><span>Main</span></div> */}
+                            </li>
+                            <li className={"sidebar__li"}>
+                                <div className={"nav__item__res"}><span>Payments</span></div>
+                            </li>
+                            <li className={"sidebar__li"}>
+                                <div className={"non__active__nav__tag"}>
+                                    <div className={"nav__content__res"}>
+                                        <img src={allPaymentIcon} alt="" className={"nav__icons"} />
+                                        <span>All Payments</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className={"sidebar__li"}>
+                                <div className={"non__active__nav__tag"}>
+                                    <div className={"nav__content__res"}>
+                                        <img src={reconcilePaymentIcon} alt="" className={"nav__icons"} />
+                                        <span>Reconcilled Payments</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className={"sidebar__li"}>
+                                <div className={"non__active__nav__tag"}>
+                                    <div className={"nav__content__res"}>
+                                        <img src={unReconsilePaymentIcon} alt="" className={"nav__icons"} />
+                                        <span>Un - Reconcilled Payments</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className={"sidebar__li"}>
+                                <div className={"non__active__nav__tag"}>
+                                    <div className={"nav__content__res"}>
+                                        <img src={manuelSettingIcon} alt="" className={"nav__icons"} />
+                                        <span>Manual Settlement</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className={"sidebar__li"}>
+                                {/* <div className={"nav__item__res"}><span>Main</span></div> */}
+                            </li>
+                            <li className={"sidebar__li"}>
+                                <div className={"nav__item__res"}><span>Orders</span></div>
+                            </li>
+                            <li className={"sidebar__li"}>
+                                <div className={"non__active__nav__tag"}>
+                                    <div className={"nav__content__res"}>
+                                        <img src={allOrdersIcon} alt="" className={"nav__icons"} />
+                                        <span>All Orders</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className={"sidebar__li"}>
+                                <div className={"non__active__nav__tag"}>
+                                    <div className={"nav__content__res"}>
+                                        <img src={pendingOrdersIcon} alt="" className={"nav__icons"} />
+                                        <span>Pending Orders</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className={"sidebar__li"}>
+                                <div className={"non__active__nav__tag"}>
+                                    <div className={"nav__content__res"}>
+                                        <img src={reconciledOrdersIcon} alt="" className={"nav__icons"} />
+                                        <span>Reconcilled Orders</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className={"sidebar__li"}>
+                                {/* <div className={"nav__item__res"}><span>Main</span></div> */}
+                            </li>
+                            <li className={"sidebar__li"}>
+                            <div className={"non__active__nav__tag"}>
+                                    <div className={"nav__content__res"}>
+                                        <img src={merchantProfileIcon} alt="" className={"nav__icons"} />
+                                        <span>Merchant Profile</span>
+                                    </div>
+                                </div>
+                            </li>
                         </ul>
                     </div>
                 </div>
                 <div className={"content__wrapper"}>
-                    body goes here
+                    <div className={"content"}>
+                        {props.children}
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/adminPage/adminPage.js
+++ src/pages/adminPage/adminPage.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import './adminPage.css';
+import cardChart from '../../assets/images/cardChart.svg';
 
 const Admin = () => {
     return (
-        <div>
-            
+        <div className={"row"} style={{padding:0, margin:0}}>
+            <div className={"col-md-6 trans__card__wrapper"}>
+                <div className={"trans__cards"}>
+                    <div className={"trans__details"}>
+                        <trans-name>Daily Transaction Volume</trans-name>
+                        <span>2,342</span>
+                    </div>
+                    <div className={"card__chart__wrapper"}>
+                        <img src={cardChart} alt="" className={"trans__card__img"} />
+                    </div>
+                </div>
+                <div className={"trans__cards"}>
+                    <div className={"trans__details"}>
+                        <trans-name>Daily Transaction Value</trans-name>
+                        <span>₦4,000,000</span>
+                    </div>
+                    <div className={"card__chart__wrapper"}>
+                        <img src={cardChart} alt="" className={"trans__card__img"} />
+                    </div>
+                </div>
+            </div>
+            <div className={"col-md-6 trans__card__wrapper"}>
+                <div className={"trans__cards"}>
+                    <div className={"trans__details"}>
+                        <trans-name>Daily Transaction Volume</trans-name>
+                        <span>2,342</span>
+                    </div>
+                    <div className={"card__chart__wrapper"}>
+                        <img src={cardChart} alt="" className={"trans__card__img"} />
+                    </div>
+                </div>
+                <div className={"trans__cards"}>
+                    <div className={"trans__details"}>
+                        <trans-name>Daily Transaction Volume</trans-name>
+                        <span>₦4,000,000</span>
+                    </div>
+                    <div className={"card__chart__wrapper"}>
+                        <img src={cardChart} alt="" className={"trans__card__img"} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
